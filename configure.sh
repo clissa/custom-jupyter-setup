17ca7ab1700e4b733b6622107cd17e6cde92c08b
Luca Clissa <luca.clissa2@unibo.it>
2021-05-21 11:15:23 +0200

    Fix bugs in configuration script

--- configure.sh
+++ configure.sh
@@ -4,14 +4,14 @@
 conda install -y -c conda-forge jupyter jupyter_contrib_nbextensions autopep8 
 
 # install nbextensions
-cp -r setup ~/.local/share/jupyter/nbextensions/
+cp -r custom_setup ~/.local/share/jupyter/nbextensions/
 jupyter contrib nbextension install --user
 
 #enable useful extensions
 jupyter nbextension enable toc2/main
 jupyter nbextension enable toggle_all_line_numbers/main
 jupyter nbextension enable excute_time/main
-jupyter nbextension enable setup/main
+jupyter nbextension enable custom_setup/main
 
 # use custom rendering options
 mkdir -p ~/.jupyter/custom
